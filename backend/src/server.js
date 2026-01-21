45933990b31ecc80acd222e946401f83648cd4ee
faresnedal1812 <nedalfares53@gmail.com>
2026-01-21 20:05:13 +0200

    application ready to deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,26 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/connectDB.js";
+import path from "path";
 
 const app = express();
+
+const __dirname = path.resolve();
+const frontendDistPath = path.join(__dirname, "../frontend", "dist");
 
 const PORT = ENV.PORT || 3000;
 
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "API is working successfully" });
 });
+
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(frontendDistPath)));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(frontendDistPath, "index.html"));
+  });
+}
 
 const startServer = async () => {
   try {
