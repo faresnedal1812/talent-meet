03f4aba7c0aa9226dfa0e24543ca6fecc1a49008
faresnedal1812 <nedalfares53@gmail.com>
2026-01-22 20:12:46 +0200

    auth and db sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,8 +2,15 @@ import express from "express";
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/connectDB.js";
 import path from "path";
+import cors from "cors";
+import { inngest, functions } from "./lib/inngest.js";
+import { serve } from "inngest/express";
 
 const app = express();
+
+app.use(express.json({ limit: "10mb" }));
+// credentials: true => mean: server is allow to browser to send cookie on the request
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 
 const __dirname = path.resolve();
 const frontendDistPath = path.join(__dirname, "../frontend", "dist");
@@ -13,6 +20,8 @@ const PORT = ENV.PORT || 3000;
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "API is working successfully" });
 });
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(frontendDistPath)));
