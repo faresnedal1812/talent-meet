dcd87ce6d0545343090f01eb0356ac60740a70d6
faresnedal1812 <nedalfares53@gmail.com>
2026-01-23 13:35:56 +0200

    auth and stream sync

---
+++ backend/src/lib/stream.js
@@ -0,0 +1,30 @@
+import { StreamChat } from "stream-chat";
+import { ENV } from "./env.js";
+
+const apiKey = ENV.STREAM_API_KEY;
+const apiSecret = ENV.STREAM_API_SECRET;
+
+if (!apiKey || !apiSecret) {
+  console.error("STREAM_API_KEY or STREAM_API_SECRET is missing");
+}
+
+export const chatClient = StreamChat.getInstance(apiKey, apiSecret); // will be used chat features
+
+// upsert => mean: create user if is not existing and update if the user is already existing
+export const upsertStreamUser = async (userData) => {
+  try {
+    await chatClient.upsertUser(userData);
+    console.log("Stream user upserted successfully:", userData);
+  } catch (error) {
+    throw new Error("Error upserting stream user", error);
+  }
+};
+
+export const deleteStreamUser = async (userId) => {
+  try {
+    await chatClient.deleteUser(userId);
+    console.log("Stream user deleted successfully:", userId);
+  } catch (error) {
+    throw new Error("Error deleting stream user", error);
+  }
+};
