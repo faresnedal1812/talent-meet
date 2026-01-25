1410920be915fd9034f4920adfb306a67762f290
faresnedal1812 <nedalfares53@gmail.com>
2026-01-25 20:03:24 +0200

    Landing page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,29 @@
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignOutButton,
-  UserButton,
-} from "@clerk/clerk-react";
+import { Routes, Route, Navigate } from "react-router";
+import HomePage from "./pages/HomePage";
+import DashboardPage from "./pages/DashboardPage";
+import ProblemsPage from "./pages/ProblemsPage";
+import { useUser } from "@clerk/clerk-react";
+import LoaderSpinner from "./components/LoaderSpinner";
 
-function App() {
+const App = () => {
+  const { isSignedIn, isLoaded } = useUser();
+  if (!isLoaded) return <LoaderSpinner />;
   return (
-    <header>
-      <SignedOut>
-        <SignInButton mode="modal" />
-      </SignedOut>
-      <SignedIn>
-        <SignOutButton />
-      </SignedIn>
-      <UserButton />
-    </header>
+    <Routes>
+      <Route
+        path="/"
+        element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />}
+      />
+      <Route
+        path="/dashboard"
+        element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />}
+      />
+      <Route
+        path="/problems"
+        element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />}
+      />
+    </Routes>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/components/LoaderSpinner.jsx
@@ -0,0 +1,15 @@
+import { motion } from "framer-motion";
+// duration: 1, repeat: Infinity, ease: "linear"
+const LoaderSpinner = () => {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-base-100 via-base-200 to-base-300 flex items-center justify-center">
+      <motion.div
+        animate={{ rotate: 360 }}
+        transition={{ duration: 1, repeat: Infinity, ease: "linear" }}
+        className="w-16 h-16 rounded-full border-4 border-primary/10 border-t-primary"
+      />
+    </div>
+  );
+};
+
+export default LoaderSpinner;
